32756113308ebfa38475c3b05afdf959d8c81853
JM <jamesmarcusjmercado@gmail.com>
2025-11-23 17:03:40 +0800

    papush

--- protected/views/layouts/column2.php
+++ protected/views/layouts/column2.php
@@ -7,16 +7,7 @@
 </div>
 <div class="span-5 last">
 	<div id="sidebar">
-	<?php
-		$this->beginWidget('zii.widgets.CPortlet', array(
-			'title'=>'Operations',
-		));
-		$this->widget('zii.widgets.CMenu', array(
-			'items'=>$this->menu,
-			'htmlOptions'=>array('class'=>'operations list-unstyled px=0'),
-		));
-		$this->endWidget();
-	?>
+	
 	</div><!-- sidebar -->
 </div>
 <?php $this->endContent(); ?>
